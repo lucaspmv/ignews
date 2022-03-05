3efe946077e84b7a3c6aa05dce278c48a2291f99
Lucas Porto <lucaspmv@hotmail.com>
2022-03-05 17:07:36 -0300

    Configuração prettier e eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
-  return (
-    <h1>Hello World</h1>
-  )
+  return <h1 className={styles.title}>Hello World</h1>;
 }
